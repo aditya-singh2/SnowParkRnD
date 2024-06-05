1326eaf707ddf49ab860217a2734ecb830416429
aditya1.singh2@fosfor.com <aditya1.singh2@fosfor.com>
2024-06-05 09:47:04 +0000

    auto save by knights-watch

--- SnowparkRnD/snow_exp.py
+++ SnowparkRnD/snow_exp.py
@@ -118,21 +118,21 @@ def train_ml_models(session: Session, exp_data: str) -> list:
         # Log the model
         model_name = f"expname_{algorithm}"
         try:
-            mv = reg.log_model(model=xgb_model,
-                               model_name=exp_details.get("name", "sample_experiment")+"_"+model_name,
+            mv = reg.log_model(model=model,
+                               model_name=exp_details.get("name", "sample_experiment")+"_"+algorithm,
                                comment="test",
                                version_name="run1",
                                python_version="3.9.19",
                                conda_dependencies=["scikit-learn==1.3.2"],
                                metrics={"model_metrics": {"MSE": mse, "MAE": mae, "r2": r2}, "project_id": "0001", "type": "EXP"})
         except Exception as ex:
-            pass
+            return ex
     return [{"EXP_NAME":exp_details.get("name", "sample_experiment"),
              "Version":"Run1",
              "matrices":{"model_metrics": {"MSE": mse, "MAE": mae, "r2": r2}, "project_id": "0001", "type": "EXP"},
              "Alogirthm_Type":"Regression",
              "Alogithms": list(exp_details.get("algo_details").keys()),
-             "RUN_STATUS":"SUCESS",
+             "RUN_STATUS":"SUCCESS",
              "registry_exp_name":""}]
 
 
